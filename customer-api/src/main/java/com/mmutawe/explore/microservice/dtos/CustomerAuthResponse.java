83f61bcf3a8415ecde467e8a4f5d8b6cef2cd13e
mmutawe <mahmoudmutawe@gmail.com>
2022-04-19 09:42:21 -0500

    implement communication between customer and cunstomer-auth APIs using RestTemplate

---
+++ customer-api/src/main/java/com/mmutawe/explore/microservice/dtos/CustomerAuthResponse.java
@@ -0,0 +1,9 @@
+package com.mmutawe.explore.microservice.dtos;
+
+import lombok.Data;
+
+@Data
+public class CustomerAuthResponse {
+    private Long customerId;
+    private Boolean isAuthorized;
+}
--- customer-api/src/main/java/com/mmutawe/explore/microservice/services/CustomerService.java
+++ customer-api/src/main/java/com/mmutawe/explore/microservice/services/CustomerService.java
@@ -1,19 +1,25 @@
 package com.mmutawe.explore.microservice.services;
 
+import com.mmutawe.explore.microservice.dtos.CustomerAuthResponse;
 import com.mmutawe.explore.microservice.dtos.CustomerRequest;
 import com.mmutawe.explore.microservice.entities.Customer;
 import com.mmutawe.explore.microservice.repositories.CustomerRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Service
 public class CustomerService {
 
     private final CustomerRepository repository;
+    private final RestTemplate restTemplate;
 
     @Autowired
-    public CustomerService(CustomerRepository repository) {
+    public CustomerService(CustomerRepository repository, RestTemplate restTemplate) {
         this.repository = repository;
+        this.restTemplate = restTemplate;
     }
 
     public Customer registerCustomer(CustomerRequest customerRequest){
@@ -24,7 +30,20 @@ public class CustomerService {
                 .email(customerRequest.getEmail())
                 .build();
 
-        repository.save(customer);
+        repository.saveAndFlush(customer);
+
+        // check if customer is authorized
+        CustomerAuthResponse customerAuthResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/auth-check/{customerId}",
+                CustomerAuthResponse.class,
+                customer.getId());
+
+        log.info("customer authorized id: {}", customerAuthResponse.getCustomerId());
+
+        if(!customerAuthResponse.getIsAuthorized()){
+//            throw new IllegalStateException("customer is not authorized");
+            log.error("customer is not authorized");
+        }
         return customer;
     }
 }
